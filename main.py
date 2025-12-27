22b71e13842326b7d7e1ad9cc13a0d152d400a1c
Faaab84 <fabien.balaramane@outlook.fr>
2025-12-27 09:21:25 +0100

    envoi

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from app.controllers.menu_controller import action_main_menu
+import sentry_sdk
+import os
+
+
+dsn_env = os.getenv("SENTRY_DSN")
+if not dsn_env:
+    raise ValueError("SENTRY_DSN environment variable is not set.")
+
+sentry_sdk.init(
+    dsn=dsn_env,
+    send_default_pii=True,
+    enable_logs=True,
+)
+
+from app.controllers.menu_controller import action_main_menu  # noqa: E402
+from app.permissions.permission import Permission  # noqa: E402
 
 
 if __name__ == "__main__":
-    action_main_menu()
-
+    if Permission().is_collaborator_authenticated():
+        from app.controllers.menu_controller import department_menu
+        department_menu()
+    else:
+        action_main_menu()
